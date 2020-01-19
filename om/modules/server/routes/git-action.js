4bb5cca3601befa5b76c27efda20ac45b44e3747
qom <qom@>
2020-01-19 16:02:50 -0500

    Add consolidated route to replace individual routes. #1

---
+++ om/modules/server/routes/git-action.js
@@ -0,0 +1,64 @@
+/*\
+title: $:/om/modules/server/routes/git-action.js
+type: application/javascript
+module-type: route
+
+GET /git/<action>
+
+action may be status, fetch, pull.
+
+\*/
+(function() {
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+	
+const simpleGit = require('simple-git/promise');
+const git = simpleGit("./tiddlers");
+
+
+exports.method = "POST";
+
+exports.path = /^\/git\/([A-z]*$)/;
+
+exports.handler = function(request,response,state) {
+	response.writeHead(200, {"Content-Type": "application/json"});
+
+	// state.params is populated with contents of the capture group in the exports.path regex
+    const action = state.params[0];
+	var result = {command: action};
+
+    git[action]()
+        .then(output => {
+            result.commandOutput = output;
+            response.end(JSON.stringify(result), "utf8");
+        })
+        .catch(error => {
+            response.writeHead(500, {"Content-Type": "application/json"});
+            result.error = error.message;
+            response.end(JSON.stringify(result), "utf8");
+        });
+};
+
+}());
+
+/*
+	git.fetch()
+		.then(data => {
+			result['fetchSummary'] = data;
+			return git.raw(['log','master..origin/master','--stat']);
+		})
+        .then(data => {
+            result['logSummary'] = data;
+            return git.status();
+        })
+		.then(data => {
+			result['statusSummary'] = data;
+			response.end(JSON.stringify(result), "utf8")
+		})
+		.catch(error => {
+			response.writeHead(500, {"Content-Type": "application/json"});
+			response.end(JSON.stringify({error: error.message}), "utf8");
+		});
+		*/
